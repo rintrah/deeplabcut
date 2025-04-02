099b70df2286381a88f3adce05c75e80c64578b6
rintrah <enriquehansen@hotmail.com>
2025-04-01 22:17:53 -0500

    Small change for locating files in Windows machine.

--- tail_tracking_zebrafish.py
+++ tail_tracking_zebrafish.py
@@ -2,10 +2,30 @@
 # light sheet recording.
 
 import deeplabcut 
+from sys import platform, exit 
+
+
+from IPython import get_ipython
+
+ipython = get_ipython()
+ipython.run_line_magic("load_ext", "autoreload")
+ipython.run_line_magic("autoreload", "1")
+
+if platform == 'linux' or platform == 'linux2':
+    video = ["/home/enrique/WashU/Data/20220218-f11/videos/2022-02-18-13-11-27.mp4"] # Enter the paths of your videos or Folder you want to grab frames from.
+    #matplotlib.use('Agg')
+elif platform == 'darwin':
+    video ["/home/enrique/WashU/Data/20220218-f11/videos/2022-02-18-13-11-27.mp4"] # Enter the paths of your videos or Folder you want to grab frames from.
+    matplotlib.use('MacOSX')
+elif platform == 'win32':
+    video = ["/home/enrique/WashU/Data/20220218-f11/videos/2022-02-18-13-11-27.mp4"] # Enter the paths of your videos or Folder you want to grab frames from.
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+else:
+    raise NameError("Unknown OS.")
 
 task			= "One-photon"  # Enter the name of your experiment Task. 
 experimenter	= "Hansen"   # Enter the name of the experimenter. 
-video			= ["/home/enrique/WashU/Data/20220218-f11/videos/2022-02-18-13-11-27.mp4"] # Enter the paths of your videos or Folder you want to grab frames from.
+
 
 # path_config_file = deeplabcut.create_new_project(
 			# task,
@@ -44,8 +64,8 @@ path_config_file = "/home/enrique/DeepLabCut/One-photon-Hansen-2025-03-27/config
 
 # deeplabcut.evaluate_network(path_config_file, plotting=True)
 
-videofile_path = "/home/enrique/WashU/Data/20220218-f11/videos/2022-02-18-13-11-27.mp4" # Enter a folder or a list of videos to analyze.
+# videofile_path = "/home/enrique/WashU/Data/20220218-f11/videos/2022-02-18-13-11-27.mp4" # Enter a folder or a list of videos to analyze.
 
-deeplabcut.analyze_videos(path_config_file, videofile_path, videotype='.mp4')
+# deeplabcut.analyze_videos(path_config_file, videofile_path, videotype='.mp4')
 
-deeplabcut.create_labeled_video(path_config_file, videofile_path)
+# deeplabcut.create_labeled_video(path_config_file, videofile_path)
